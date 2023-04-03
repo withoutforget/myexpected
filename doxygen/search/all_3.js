3d9d527fa055dd6c59e0ee0f585bc2c75a97966f
withoutforget <89633210+withoutforget@users.noreply.github.com>
2023-04-03 17:59:18 +0300

    Add files via upload

---
+++ doxygen/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['has_5ferror_0',['has_error',['../classexpected.html#a99b5ba3ad30563909ac3954d47778c70',1,'expected::has_error()'],['../classexpected.html#a238048418f5c6935af7e355ea4b7d892',1,'expected::has_error() const']]],
+  ['has_5fvalue_1',['has_value',['../classexpected.html#ac4fd9862fb40b0775113beaa11adf139',1,'expected::has_value()'],['../classexpected.html#ab23add9338ec307c59be3af1d992aa8e',1,'expected::has_value() const']]]
+];
---
+++ doxygen/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['operator_3d_0',['operator=',['../classexpected.html#ab93ff0baa3e5624d747c683ed1fac1d0',1,'expected::operator=(const expected&lt; T, U &gt; &amp;rhs)'],['../classexpected.html#a46c2ad8e5ba26a56fef9010ff793e998',1,'expected::operator=(expected&lt; T, U &gt; &amp;&amp;rhs) noexcept']]]
+];
